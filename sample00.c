426407921ed9d162c54dfa4f34ff408e5073a586
Kouichi Shiroma <shiroma@stlc.co.jp>
2015-09-14 00:13:58 +0900

    changed a file name.

---
+++ sample00.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "sfs.h"
+
+void task1(void)
+{
+	printf("task1!\n");
+	return;
+}
+void task2(void)
+{
+	printf("task2!\n");
+	return;
+}
+int main(void)
+{
+	int loop=10;
+
+	SFS_initialize();
+
+	SFS_fork("TASK1",0,task1);
+	SFS_fork("TASK2",0,task2);
+
+	while(loop--)
+		SFS_dispatch();
+
+	return 1;
+}
